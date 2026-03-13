c2ed51f47d0160dda692b6b21b371cb59a645f85
Seb Harvey <90053067+sebharvey@users.noreply.github.com>
2026-03-13 11:04:47 +0000

    Update Program.cs

--- src/API/Template/TemplateApi/Program.cs
+++ src/API/Template/TemplateApi/Program.cs
@@ -4,7 +4,7 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SevernValleyTimetable.Functions;
+using ReservationSystem.Template.Functions;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
